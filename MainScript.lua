0575426bc56355b794d6c83ce5f69c83fde7f62a
SubnauticaLaserMain <123651736+SubnauticaLaserMain@users.noreply.github.com>
2024-05-04 20:45:23 +0200

    Create MainScript.lua

---
+++ MainScript.lua
@@ -0,0 +1,31 @@
+--[[ // Service(s) \\]]
+local CorePackages = game:GetService('CorePackages')
+local PlayerService = game:GetService('Players')
+local CoreGui = game:GetService('CoreGui')
+
+
+
+--[[// Roblox Service(s) \\]]
+---local Roact = getrenv().require(CorePackages.Roact)
+
+
+
+local function HttpGet(HttpLink, Boolean)
+    return game:HttpGet(HttpLink, (Boolean or false))
+end
+
+
+
+
+
+local GamesSupported = ({
+    -- In Game (Break In 2)
+    [13864667823] = HttpGet('https://raw.githubusercontent.com/SubnauticaLaserMain/ServerScriptAPI-Android/main/BreakIn2-InGame.lua', true),
+    -- Lobby (Break In 2)
+    [13864661000] = HttpGet('https://github.com/SubnauticaLaserMain/ServerScriptAPI-Android/blob/main/BreakIn2-Lobby.lua', true)
+})[game.PlaceId]
+
+
+if GamesSupported ~= nil then
+    loadstring(GamesSupported)
+end
